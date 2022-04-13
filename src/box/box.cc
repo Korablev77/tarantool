677dad9cf0b782375db14bf2866662973982c523
Nikita Pettik <korablev@tarantool.org>
2022-04-13 11:34:17 +0300

    box: add OS stubs for flight recorder logger
    Let's add no-op interface functions and box cfg default parameters.
    
    NO_DOC=<No user visible changes>
    NO_TEST=<No functional changes>
    NO_CHANGELOG=<No functional changes>

--- src/box/box.cc
+++ src/box/box.cc
@@ -86,6 +86,7 @@
 #include "trivia/util.h"
 #include "version.h"
 #include "mp_uuid.h"
+#include "flightrec.h"
 
 static char status[64] = "unknown";
 
@@ -764,6 +765,18 @@ box_check_audit(void)
 	}
 }
 
+/**
+ * Raises error if flight recorder configuration is incorrect.
+ */
+static void
+box_check_flightrec(void)
+{
+	if (flightrec_check_cfg(cfg_geti64("flightrec_logs_size"),
+				cfg_geti64("flightrec_logs_max_msg_size"),
+				cfg_geti("flightrec_logs_log_level")) != 0)
+		diag_raise();
+}
+
 static enum election_mode
 box_check_election_mode(void)
 {
@@ -1319,6 +1332,7 @@ box_check_config(void)
 	struct tt_uuid uuid;
 	box_check_say();
 	box_check_audit();
+	box_check_flightrec();
 	if (box_check_listen() != 0)
 		diag_raise();
 	box_check_instance_uuid(&uuid);
@@ -3257,6 +3271,7 @@ box_free(void)
 		gc_free();
 		engine_shutdown();
 		wal_free();
+		flightrec_free();
 		audit_log_free();
 		sql_built_in_functions_cache_free();
 	}
