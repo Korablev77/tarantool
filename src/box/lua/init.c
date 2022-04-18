e917ba8938eeed43cdb65ee0768e4c75cc80627b
Nikita Pettik <korablev@tarantool.org>
2022-04-18 16:03:37 +0300

    box: add OS stubs for flight recorder metrics
    Let's add no-op interface functions and box cfg default parameters.
    
    NO_DOC=<No user visible changes>
    NO_TEST=<No functional changes>
    NO_CHANGELOG=<No functional changes>

--- src/box/lua/init.c
+++ src/box/lua/init.c
@@ -89,6 +89,9 @@ extern char session_lua[],
 #if ENABLE_AUDIT_LOG
 	audit_lua[],
 #endif
+#if ENABLE_FLIGHT_RECORDER
+	flightrec_lua[],
+#endif
 	net_box_lua[],
 	upgrade_lua[],
 	console_lua[],
@@ -108,6 +111,9 @@ static const char *lua_sources[] = {
 #endif
 #if ENABLE_AUDIT_LOG
 	"box/audit", audit_lua,
+#endif
+#if ENABLE_FLIGHT_RECORDER
+	"box/flightrec", flightrec_lua,
 #endif
 	"box/upgrade", upgrade_lua,
 	"box/net_box", net_box_lua,
